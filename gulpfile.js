bf83b7aa70445aefe776c90d22eb3c8c7d643b80
znss1989 <znss1989@gmail.com>
2016-09-09 11:12:32 +0800

    reactify to babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
     browserify('./src/js/main.js')
-        .transform('reactify')
+        .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
         .pipe(source('main.js'))
         .pipe(gulp.dest('dist/js'));
